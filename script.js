1e1c6ca3a3ec688ecbc3f0d028cd0cb72cbe5c0b
Christian Cuadero <christiancuadero@gmail.com>
2024-05-22 15:55:09 +0800

    update html and js
    updated script.js for qr code to show when page is initially loaded.

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-  
-const qrCode = new QRCodeStyling({
-    width: 200,
-    height: 200,
-    data: "https://www.frontendmentor.io/",
-        
-    dotsOptions: {
-    color: "#FFFFFF",
-    type: "square"
-},
-    backgroundOptions: {
-    color: "#2C7DFA",
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const qrCode = new QRCodeStyling({
+        width: 200,
+        height: 200,
+        data: "https://www.frontendmentor.io/",
+        dotsOptions: {
+            color: "#FFFFFF",
+            type: "square"
+        },
+        backgroundOptions: {
+            color: "#2C7DFA"
+        }
+    });
+
+    qrCode.append(document.getElementById("qrcode"));
 });
-qrCode.append(document.getElementById("qrcode"));
